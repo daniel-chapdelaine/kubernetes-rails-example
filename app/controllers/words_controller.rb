c4ae5983c9d4b5a0857e85a1c0edc26d7e0fb30f
Daniel Chapdelaine <daniel.chapdelaine@vitalsource.com>
2022-08-18 09:38:58 -0500

    using db locally

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,6 +1,16 @@
 class WordsController < ApplicationController
+  self.per_form_csrf_tokens = true
+
 
   def index
+    @word = Word.new
+    @words = Word.all.pluck(:text)
+  end
+
+  def new
+    @word = Word.new(params.permit(:text))
+    @word.save
+    redirect_to "/"
   end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   get "/", to: "words#index"
+  post "/words/new", to: "words#new"
 end
